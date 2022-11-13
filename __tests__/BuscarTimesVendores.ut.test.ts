6d55a01ebcba57b7bc4040db8204b6c3ffc57c35
fernando-aramis-99 <fernandoaramis.99@gmail.com>
2022-11-13 16:12:10 -0300

    add test

--- __tests__/BuscarTimesVendores.ut.test.ts
+++ __tests__/BuscarTimesVendores.ut.test.ts
@@ -2,19 +2,19 @@ import { CopaController } from '../src/controller/copaController';
 import { prismaMock } from '../singleton'
 import input from "../temp/dadosParaSalvarNoDB.json"
 
-describe("Teste do Buscar todos os times", () => {
+describe("Teste do Buscar times vencedores", () => {
     afterEach(() => {
         jest.resetAllMocks();
     })
 
     const copaController = new CopaController()
 
-    test('Buscar todos os times... Teste', async () => {
+    test('Buscar times vencedores... Teste ', async () => {
       
         // @ts-ignore
         prismaMock.times.findMany.mockRejectedValue(input)
 
-        const result = await copaController.buscarTodosOsTimes()
+        const result = await copaController.buscarTimesVendores()
         expect(result).toBeDefined()
       })
 })
--- __tests__/BuscarVencedoresDosMatchDaysNoDb.ut.test.ts
+++ __tests__/BuscarVencedoresDosMatchDaysNoDb.ut.test.ts
@@ -2,19 +2,19 @@ import { CopaController } from '../src/controller/copaController';
 import { prismaMock } from '../singleton'
 import input from "../temp/dadosParaSalvarNoDB.json"
 
-describe("Teste do Buscar todos os times", () => {
+describe("Teste do buscar Vencedores Dos MatchDays No Db", () => {
     afterEach(() => {
         jest.resetAllMocks();
     })
 
     const copaController = new CopaController()
 
-    test('Buscar todos os times... Teste', async () => {
+    test('buscar Vencedores Dos MatchDays No Db... Teste ', async () => {
       
         // @ts-ignore
         prismaMock.times.findMany.mockRejectedValue(input)
 
-        const result = await copaController.buscarTodosOsTimes()
+        const result = await copaController.buscarVencedoresDosMatchDaysNoDb()
         expect(result).toBeDefined()
       })
 })
